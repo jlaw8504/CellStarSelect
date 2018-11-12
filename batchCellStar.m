26e365052eb55142aa4cc5e82bf85db231d7af51
jlaw8504 <lawrimor@email.unc.edu>
2018-11-12 14:49:39 -0500

    Addedj batchCellStar.m

---
+++ batchCellStar.m
@@ -0,0 +1,27 @@
+function parameters =  batchCellStar(...
+                            fullFilenamesCell,...
+                            destDirSeg, bgImageFullFilename)
+%%Run CellStar in batch mode
+% Inputs :
+%       fullFilenames : Cell array containing the full filnames
+%       (filpath/filename) of all the DIC images to segment using CellStar
+%
+%       destDirSeg : Directory where the segmentation mat files and the
+%       images will be stored
+%
+%       bgImageFullFilename : String of the full filename of the DIC image
+%       of the background (no cells in the image)
+
+%Initialize paramters structure array for CellStar segmentation
+%Given pixel size of ~64.5, an 'average' cell diameter of 95 equals
+%roughly 6 microns
+parameters = DefaultParameters('precision', 7, 'avgCellDiameter', 95);
+parameters.debugLevel = 2;
+parameters.files.destinationDirectory = destDirSeg;
+parameters.files.background.imageFile = bgImageFullFilename;
+parameters.files.imagesFiles = fullFilenamesCell;
+%specifiy the maximum number of clusters on current parpool profile
+clusterProfileStruct = parcluster;
+parameters.maxThreads = clusterProfileStruct.NumWorkers;
+parameters = CompleteParameters(parameters);
+parameters = RunSegmentation(parameters);
