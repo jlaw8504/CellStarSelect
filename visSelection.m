436a68ac4e7b7900b6eb4dd5e2cb8b55eaf92d32
Josh Lawrimore <lawrimor@email.unc.edu>
2018-11-26 13:01:39 -0500

    Updated visSelection.m
    Pre-padded all image planes to prevent indexing errors while cropping. Fixed the scatterplot indexing. Centered cropped image planes on the first spot of the first image matrix.

--- visSelection.m
+++ visSelection.m
@@ -7,23 +7,33 @@ for n=2:size(dataCell,1)
         subplot(1,4,i);
         display(n);
         plane = dataCell{n,i}(3);
-        X = dataCell{n,i}(2);
-        Y = dataCell{n,i}(1);
-        if i < 3
-            try
-                imshow(img1Mat(Y-49:Y+49,X-49:X+49,plane),[]);
-            catch
-                imshow(img1Mat(Y-24:Y+24,X-24:X+24,plane),[]);
-            end
-        else
-            try
-                imshow(img2Mat(Y-49:Y+49,X-49:X+49,plane),[]);
-            catch
-                imshow(img2Mat(Y-24:Y+24,X-24:X+24,plane),[]);
-            end
+        if i == 1 %to center image around first channel's first spot
+            midX = dataCell{n,i}(2) + 50; %adjusted by 50 for padding of image
+            midY = dataCell{n,i}(1) + 50;
         end
+        %To prevent indexing errors, pad image with 50 edge pixel value
+        if i < 3
+            imgPlane = img1Mat(:,:,plane);
+        else
+            imgPlane = img2Mat(:,:,plane);
+        end
+        imgPlane = padarray(imgPlane,[50, 50],'replicate','both');
+        imshow(imgPlane(midY-50:midY+50, midX-50:midX+50), []);
         hold on;
-        scatter(dataCell{n,i}(1), dataCell{n,i}(2));
+        %parse out and adjust coorindates of each spot
+        X = dataCell{n,i}(2) + 50;
+        Y = dataCell{n,i}(1) + 50;
+        switch i
+            case 1
+                marker = 'go';
+            case 2
+                marker = 'go';
+            case 3
+                marker = 'rx';
+            case 4
+                marker = 'rx';
+        end
+        scatter(X - midX + 51, Y - midY + 51, marker);
         hold off;
         if i == 1
             title('Image 1, Spot 1');
